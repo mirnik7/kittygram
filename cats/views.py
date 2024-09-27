ce3720331f99b713f34e6a70efc0c9f67f117e19
Miroslav Nikolaichenko <mirnik7thebest@yandex.com>
2024-09-27 16:18:04 +0300

    1

--- cats/views.py
+++ cats/views.py
@@ -9,11 +9,34 @@ from .serializers import CatSerializer
 @api_view(['GET', 'POST'])
 def cat_list(request):
     if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
+        serializer = CatSerializer(data=request.data, many=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def cat_detail(request, pk):
+    cat = Cat.objects.get(pk=pk)
+    if request.method == 'PATCH':
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'PUT':
+        serializer = CatSerializer(cat, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        cat.delete()
+        return Response()
+    
+    serializer = CatSerializer(cat)
     return Response(serializer.data)
